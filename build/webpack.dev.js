d6eff7b8e4c310535b3b6f275a0534cb79086855
fuchen <284992984@qq.com>
2020-06-16 11:17:55 +0800

    修改webpack配置

--- build/webpack.dev.js
+++ build/webpack.dev.js
@@ -1,124 +1,131 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const VueLoaderPlugin = require('vue-loader/lib/plugin')
-const FriendlyErrorsWebpackPlugin = require('friendly-errors-webpack-plugin');
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const VueLoaderPlugin = require("vue-loader/lib/plugin");
+const FriendlyErrorsWebpackPlugin = require("friendly-errors-webpack-plugin");
 
 module.exports = {
-  mode: 'development',
+  mode: "development",
   cache: true,
-  context: path.resolve(__dirname, '../'),
-  entry: './src/main.js',
+  context: path.resolve(__dirname, "../"),
+  entry: "./src/main.js",
   output: {
-    filename: '[name].js',
-    path: path.resolve(__dirname, '../dist'),
+    filename: "[name].js",
+    path: path.resolve(__dirname, "../dist"),
   },
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, '../src')
+      "@": path.resolve(__dirname, "../src"),
     },
-    extensions: ['.js', '.vue', '.json'],
-    modules: [path.resolve(__dirname, '../node_modules')]
+    extensions: [".js", ".vue", ".json"],
+    modules: [path.resolve(__dirname, "../node_modules")],
   },
   module: {
-    rules:[
+    noParse: /^(vue|vue-router|vuex|vuex-router-sync)$/, // 忽略大型的 library 可以提高构建性能
+    rules: [
       {
         test: /\.vue$/,
         use: [
-          'cache-loader',
+          "cache-loader",
           {
-            loader: 'vue-loader',
+            loader: "vue-loader",
             options: {
               compilerOptions: {
-                preserveWhitespace: false
-              }
-            }
-          }
-        ]
+                preserveWhitespace: false,
+              },
+            },
+          },
+        ],
       },
       {
         test: /\.js$/,
         use: [
           {
-            loader: 'babel-loader',
+            loader: "babel-loader",
             options: {
-              cacheDirectory: true
-            } 
-          }
+              cacheDirectory: true,
+            },
+          },
         ],
-        include: path.resolve(__dirname, '../src'),
+        include: path.resolve(__dirname, "../src"),
       },
       {
         test: /\.css$/,
-        use: [
-          'vue-style-loader',
-          'css-loader',
-          'postcss-loader',
-        ],
+        use: ["vue-style-loader", "css-loader", "postcss-loader"],
         exclude: /node_modules/,
       },
       {
         test: /\.scss$/,
         use: [
-          'vue-style-loader',
-          'css-loader',
-          'postcss-loader',
-          'sass-loader'
+          "vue-style-loader",
+          "css-loader",
+          "postcss-loader",
+          "sass-loader",
         ],
         exclude: /node_modules/,
       },
       {
         test: /\.(png|jpe?g|gif|webp)(\?.*)?$/,
-        loader: 'url-loader',
+        loader: "url-loader",
         options: {
           limit: 1024,
           esModule: false,
           fallback: {
-            loader: require.resolve('file-loader'),
+            loader: require.resolve("file-loader"),
             options: {
-              name: "img/[name].[hash:8].[ext]"
-            }
-          }
-        }
+              name: "img/[name].[hash:8].[ext]",
+            },
+          },
+        },
       },
       {
         test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
-        loader: 'url-loader',
+        loader: "url-loader",
         options: {
           limit: 1024,
           esModule: false,
           fallback: {
-            loader: require.resolve('file-loader'),
+            loader: require.resolve("file-loader"),
             options: {
-              name: "media/[name].[hash:8].[ext]"
-            }
-          }
-        }
+              name: "media/[name].[hash:8].[ext]",
+            },
+          },
+        },
       },
       {
         test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/i,
-        loader: 'url-loader',
+        loader: "url-loader",
         options: {
           limit: 1024,
           esModule: false,
           fallback: {
-            loader: require.resolve('file-loader'),
+            loader: require.resolve("file-loader"),
             options: {
-              name: "fonts/[name].[hash:8].[ext]"
-            }
-          }
-        }
-      }
-    ]
+              name: "fonts/[name].[hash:8].[ext]",
+            },
+          },
+        },
+      },
+    ],
   },
   plugins: [
-    new FriendlyErrorsWebpackPlugin(),
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, '../public/index.html'),
-      filename: 'index.html',
+    new webpack.DefinePlugin({
+      "process.env": {
+        NODE_ENV: JSON.stringify(process.env.NODE_ENV || ""),
+      },
     }),
-    new VueLoaderPlugin()
+    new FriendlyErrorsWebpackPlugin({
+      compilationSuccessInfo: {
+        messages: [`App running at: http://localhost:8080`],
+      },
+    }),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "../public/index.html"),
+      filename: "index.html",
+    }),
+    new VueLoaderPlugin(),
   ],
-  devtool: 'cheap-module-eval-source-map',
+  devtool: "cheap-module-eval-source-map",
   devServer: {
     open: true,
     hot: true,
@@ -126,8 +133,8 @@ module.exports = {
     compress: true,
     overlay: {
       warnings: true,
-      errors: true
+      errors: true,
     },
-    quiet: true
+    quiet: true,
   },
-}
+};
